d3f680c29debc6e3cc12f401849c98496e44a415
Blair <blair300.bf@gmail.com>
2021-06-26 02:17:11 +0100

    page

--- header.php
+++ header.php
@@ -4,5 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>PeachPress</title>
+	<?php wp_head(); ?>
 </head>
 <body>
+	<?php wp_body_open(); ?>
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-<?php if have_posts() : while( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <?php endwhile; else : ?>
 	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
 
-<?php get_header(); ?>
+<?php get_footer(); ?>
