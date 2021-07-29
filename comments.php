a039e8e1b4a370eb8ce669a9d1f61729b7060e8f
blair3003 <blair3003.bf@gmail.com>
2021-07-29 12:55:22 +0100

    comments list

--- comments.php
+++ comments.php
@@ -9,20 +9,28 @@ if ( ! defined( 'ABSPATH' ) ) : exit; endif;
 ?>
 
 <section id="comments" class="comments">
-	<div class="comments-form">		
-		<?php
-		comment_form(
-			array(
-				'fields' => array(
-					'author' => '<p class="comment-form-author"><label for="author" class="offscreen">Name *</label><input id="author" name="author" type="text" size="30" maxlength="245" placeholder="Name *"/></p>',
-					'email' => '<p class="comment-form-email"><label for="email" class="offscreen">Email *</label><input id="email" name="email" type="text" size="30" maxlength="245" placeholder="Email *"/></p>',
-					'url' => '<p class="comment-form-url"><label for="url" class="offscreen">Website</label><input id="url" name="url" type="text" size="30" maxlength="200" placeholder="Website"/></p>',
-				),
-				'title_reply' => 'Leave a Comment',
-				'comment_notes_before' => null,
-			)
-		);
-		?>		
-	</div>
+	<h2 class="comments-title">Comments</h2>
+
+	<?php if ( have_comments() ) : ?>
+	    <ol class="comments-list">
+	    	<?php wp_list_comments(); ?>
+		</ol>
+	<?php endif; ?>
+
+	<?php
+	comment_form(
+		array(
+			'fields' => array(
+				'author' => '<p class="comment-form-author"><label for="author" class="offscreen">Name *</label><input id="author" name="author" type="text" size="30" maxlength="245" placeholder="Name *"/></p>',
+				'email' => '<p class="comment-form-email"><label for="email" class="offscreen">Email *</label><input id="email" name="email" type="text" size="30" maxlength="245" placeholder="Email *"/></p>',
+				'url' => '<p class="comment-form-url"><label for="url" class="offscreen">Website</label><input id="url" name="url" type="text" size="30" maxlength="200" placeholder="Website"/></p>',
+			),
+			'title_reply' => 'Leave a Comment',
+			'comment_notes_before' => null,
+			'class_container' => 'comments-form'
+		)
+	);
+	?>		
+
 
 </section>
