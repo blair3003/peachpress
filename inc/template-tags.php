490a93810b2b5e17e485f3ec032b2045c3863d20
blair3003 <blair3003.bf@gmail.com>
2021-07-30 12:50:33 +0100

    comment style

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -21,32 +21,34 @@ if ( ! function_exists('peachpress_comment') ) {
         ?>
 
         <li id="comment-<?php comment_ID(); ?>" <?php comment_class( $args['has_children'] ? 'parent' : '' ); ?>>
-            <div class="comment-thumbnail"><?php echo get_avatar($comment) ?></div>
-            <div class="comment-body">
-                <div class="comment-meta">
-                    <cite itemprop="name" class="fn"><?php echo get_comment_author_link(); ?></cite>
-                    <time datetime="<?php comment_time('c'); ?>" itemprop="datePublished">
-                        <?php echo human_time_diff( get_comment_time('U'), current_time('timestamp') ) . ' ago'; ?>
-                    </time>
-                </div>
-                <div class="comment-content"><?php comment_text(); ?></div>
-                <div class="comment-footer">
-                    <?php
-                    edit_comment_link( 'Edit', '<span class="edit-link">', '</span>' );
-                    comment_reply_link(
-                        array_merge(
-                            $args,
-                            array(
-                                'depth'     => $depth,
-                                'max_depth' => $args['max_depth'],
-                                'before'    => '<span class="reply">',
-                                'after'     => '</span>',
+            <article class="comment-single">
+                <div class="comment-thumbnail"><?php echo get_avatar($comment) ?></div>
+                <div class="comment-body">
+                    <div class="comment-meta">
+                        <cite itemprop="name" class="comment-name"><?php echo get_comment_author_link(); ?></cite>
+                        <time datetime="<?php comment_time('c'); ?>" itemprop="datePublished" class="comment-time">
+                            <?php echo human_time_diff( get_comment_time('U'), current_time('timestamp') ) . ' ago'; ?>
+                        </time>
+                    </div>
+                    <div class="comment-content"><?php comment_text(); ?></div>
+                    <div class="comment-footer">
+                        <?php
+                        edit_comment_link( 'Edit', '<span class="edit-link">', '</span>' );
+                        comment_reply_link(
+                            array_merge(
+                                $args,
+                                array(
+                                    'depth'     => $depth,
+                                    'max_depth' => $args['max_depth'],
+                                    'before'    => '<span class="reply">',
+                                    'after'     => '</span>',
+                                )
                             )
-                        )
-                    );
-                    ?>
-                </div>
-            </div>
+                        );
+                        ?>
+                    </div>
+                </div>                
+            </article>
 
         <?php
 
