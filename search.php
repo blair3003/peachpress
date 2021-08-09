1ab0ac419ac9d30ca0255d8a3b23d6f8dc1b8736
blair3003 <blair3003.bf@gmail.com>
2021-08-09 11:02:57 +0100

    search

--- search.php
+++ search.php
@@ -12,7 +12,29 @@ if ( ! defined( 'ABSPATH' ) ) : exit; endif;
 
 <main class="site-main">
 
-Search results
+    <?php
+    if ( have_posts() ) :
+    ?>
+    <header class="page-header">
+        <h1 class="page-title">
+            Search results for: <?php echo esc_html( the_search_query() ); ?>
+        </h1>    
+    </header>
+
+    <?php
+        while ( have_posts() ) :
+            the_post();
+            get_template_part( 'template-parts/content/content', 'excerpt' );
+        endwhile;
+        the_posts_pagination( array(
+            'mid_size'  => 2,
+            'prev_text' => '← Previous',
+            'next_text' => 'Next →',
+        ) );
+    else :
+        get_template_part( 'template-parts/content/content', 'none' );
+    endif;
+    ?>
 
 </main>
 
